dbdf9958b006cbdbf3a71dbd492a120ef5077069
Benjamin Davies <B.I.Davies@lboro.ac.uk>
2020-03-02 13:47:12 +0000

    Add files via upload
    Initial Template

---
+++ GroupCoursework/GroupCoursework/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GroupCoursework
+//
+//  Created by Ben Davies on 02/03/2020.
+//  Copyright © 2020 Loughborough University. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
